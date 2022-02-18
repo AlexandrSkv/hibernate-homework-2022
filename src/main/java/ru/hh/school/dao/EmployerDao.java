cd1ca6b8b834e13dadf66624614b76177f0a9e79
sasha_rus <sasha_rus@list.ru>
2022-02-18 13:39:18 +0300

    fix

--- src/main/java/ru/hh/school/dao/EmployerDao.java
+++ src/main/java/ru/hh/school/dao/EmployerDao.java
@@ -19,7 +19,7 @@ public class EmployerDao extends GenericDao {
   public Employer getEager(int employerId) {
     return getSession()
         .createQuery("from Employer employer " +
-                "join fetch employer.vacancies " +
+                "left join fetch employer.vacancies " +
                 "where employer.id = :employerId", Employer.class)
             .setParameter("employerId", employerId)
             .getSingleResult();
